fcd469020e3c7e56b43fc46fcab05445ee27a7bb
Felipe Alves da Silva <felipe.silva.devtech@gmail.com>
2023-03-22 18:15:53 -0300

    create custom colors to theme

---
+++ cms/src/admin/app.ts
@@ -0,0 +1,26 @@
+export default {
+  config: {
+    locales: ["pt-BR"],
+    theme: {
+      dark: {
+        colors: {
+          primary100: "red",
+          primary200: "red",
+          primary500: "red",
+          primary600: "red",
+          primary700: "red",
+        },
+      },
+      light: {
+        colors: {
+          primary100: "red",
+          primary200: "red",
+          primary500: "red",
+          primary600: "red",
+          primary700: "red",
+        },
+      },
+    },
+  },
+  bootstrap() {},
+};
